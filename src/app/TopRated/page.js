f7353ff3637de3b245b4946b8b69453a1329e908
25LP7668 25LP7668 <25LP7668@Tuvshins-iMac.local>
2025-09-29 09:33:46 +0800

    sent

--- src/app/TopRated/page.js
+++ src/app/TopRated/page.js
@@ -17,7 +17,7 @@ const options = {
 };
 export default function TopRated() {
   const [topRatedMoviesData, setTopRatedMoviesData] = useState([]);
-  
+
   const getData = async () => {
     const data = await fetch(apiLink, options);
     const jsonData = await data.json();
@@ -46,7 +46,7 @@ export default function TopRated() {
                 <MovieCard
                   key={index}
                   title={movie.title}
-                  imgSrc={`https://image.tmdb.org/t/p/original/${movie.posterp_path}`}
+                  imgSrc={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                   rating={movie.rating}
                 />
               );
@@ -56,7 +56,7 @@ export default function TopRated() {
                 <MovieCard
                   key={index}
                   title={movie.title}
-                  imgSrc={`https://image.tmdb.org/t/p/original/${movie.posterp_path}`}
+                  imgSrc={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                   rating={movie.rating}
                 />
               );
@@ -64,20 +64,33 @@ export default function TopRated() {
           </div>
         </div>
       </div>
-      <div className="mt-5 mb-5">
+      <div className="mt-5 mb-5 ">
         <div className="w-[1280px] h-[40px] flex justify-end">
-        <div className="w-[382px] h-[40px] flex flex-row gap-[3px]">
-            <button className="w-[114px] h-[40px] flex justify-center items-center text-[#09090B]"><img src="./paginationVector" className="w-[5px] h-[5px]"/>Previous</button>
-             <div className="w-[172px] h-[40px] flex flex-row gap-[3px]">
-            <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">1</button>
-            <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">2</button>
-            <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">...</button>
-            <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">5</button>
+          <div className="w-[382px] h-[40px] flex flex-row gap-[3px]">
+            <button className="w-[114px] h-[40px] flex justify-center items-center text-[#09090B]">
+              <img src="./paginationVector" className="w-[5px] h-[5px]" />
+              Previous
+            </button>
+            <div className="w-[172px] h-[40px] flex flex-row gap-[3px]">
+              <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">
+                1
+              </button>
+              <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">
+                2
+              </button>
+              <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">
+                ...
+              </button>
+              <button className="w-[40px] h-[40px] flex justify-center items-center text-black text-[14px] rounded-md  ">
+                5
+              </button>
             </div>
-            <button className="w-[88px] h-[40px] flex justify-center items-center text-black">Next<img src="./paginationNextVector" className="w-[5px] h-[5px]"/></button>
+            <button className="w-[88px] h-[40px] flex justify-center items-center text-black">
+              Next
+              <img src="./paginationNextVector" className="w-[5px] h-[5px]" />
+            </button>
+          </div>
         </div>
-       
-    </div>
       </div>
       <FooterContent />
     </div>
